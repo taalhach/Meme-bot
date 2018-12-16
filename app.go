dbd51c2d8010ea9e6a5a6151864483640a165a10
taalhach <talhach891@gmail.com>
2018-12-16 06:52:38 +0500

    changed main file flder

--- app.go
+++ app.go
@@ -65,7 +65,7 @@ func VerificationEndpoint(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(" challenge: ",challenge)
 	mode := r.URL.Query().Get("hub.mode")
 	fmt.Println(" mode: ",mode)
-	token := r.URL.Query().Get("VERIFY_TOKEN")
+	token := r.URL.Query().Get("hub.verify_token")
 
 	fmt.Println(os.Getenv("VERIFY_TOKEN"))
 	fmt.Println("query: ",r.URL.Query())
